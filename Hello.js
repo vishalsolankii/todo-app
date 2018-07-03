8a6817e60f01460782dc5781b354f3d3f6c857e9
Vishal <40351247+vishalsolankii@users.noreply.github.com>
2018-07-03 17:25:27 +0530

    helloo world

---
+++ Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+class helloo extends Component {
+  render() {
+    return (
+     
+          <h1 className="wel">Welcome Vishal solanki </h1>
+     
+    );
+  }
+}
+
+export default helloo;
+   
